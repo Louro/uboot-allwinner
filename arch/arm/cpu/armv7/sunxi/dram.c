f005f8fa18ec4790a1beddd8d6a6e74c0a240d16
Tom Cubie <tangliang@allwinnertech.com>
2012-10-26 00:56:57 +0200

    ARM: sunxi: Basic Allwinner A10/A13 (sun4i/sun5i) support
    This patch adds support for the Allwinner A10/A13 SoC's. Additionally
    board support for the dev-boards sun4i/sun5i is added.
    
    NOTES (remove in final version before submission):
    Additionally, I did put some comments for further cleanup into the
    code. Search for "test-only" to find those.
    
    I also added Tom Cubie as original author and added your s-o-b as you will
    notice. I hope this is correct. Otherwise just change it.
    
    Still missing it a maintainer entry for those boards in the
    MAINTAINERS file. Is this Tom Cubie? Or you?
    
    And should Tom's email address be changed? He is not working for Allwinner
    any more. Correct?
    
    Signed-off-by: Tom Cubie <tangliang@allwinnertech.com>
    Signed-off-by: Henrik Nordström <henrik@henriknordstrom.net>
    Signed-off-by: Stefan Roese <sr@denx.de>

---
+++ arch/arm/cpu/armv7/sunxi/dram.c
@@ -0,0 +1,73 @@
+/*
+ * (C) Copyright 2007-2012
+ * Allwinner Technology Co., Ltd. <www.allwinnertech.com>
+ * Berg Xing <bergxing@allwinnertech.com>
+ * Tom Cubie <tangliang@allwinnertech.com>
+ *
+ * Sunxi platform dram controller init.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <asm/arch/dram.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/sys_proto.h>
+
+// test-only: no volatile access, this should be converted to readl/writel
+#define mctl_read_w(n)      (*((volatile unsigned int *)(n)))
+#define mctl_write_w(n,c)   (*((volatile unsigned int *)(n)) = (c))
+
+// test-only: no uppercase function and variable names
+/*
+ * Get DRAM Size in MB unit;
+ */
+unsigned DRAMC_get_dram_size(void)
+{
+	__u32 reg_val;
+	__u32 dram_size;
+	__u32 chip_den;
+
+	reg_val = mctl_read_w(SDR_DCR);
+	chip_den = (reg_val >> 3) & 0x7;
+
+	// test-only: use an algorythm, like dram_size = (32 << chip_den)
+	if (chip_den == 0)
+		dram_size = 32;
+	else if (chip_den == 1)
+		dram_size = 64;
+	else if (chip_den == 2)
+		dram_size = 128;
+	else if (chip_den == 3)
+		dram_size = 256;
+	else if (chip_den == 4)
+		dram_size = 512;
+	else
+		dram_size = 1024;
+
+	if (((reg_val >> 1) & 0x3) == 0x1)
+		dram_size <<= 1;
+	if (((reg_val >> 6) & 0x7) == 0x3)
+		dram_size <<= 1;
+	if (((reg_val >> 10) & 0x3) == 0x1)
+		dram_size <<= 1;
+
+	return dram_size;
+}
